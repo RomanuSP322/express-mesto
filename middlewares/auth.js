a0cbc0cf529dacb141fff1a34aebf820d0ed31be
Roman SP <prs@m-cti.ru>
2021-06-19 19:51:57 +0300

    bugfix

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -6,10 +6,10 @@ const { NODE_ENV, JWT_SECRET } = process.env;
 // eslint-disable-next-line consistent-return
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
-  if (!authorization && !authorization.startsWith('Bearer ')) {
+  if (!authorization && !authorization.startsWith('Bearer')) {
     throw new UnauthorizedError('Войдите на сайт');
   }
-  const token = authorization.replace('Bearer ', '');
+  const token = authorization.replace('Bearer', '');
   let payload;
 
   try {
